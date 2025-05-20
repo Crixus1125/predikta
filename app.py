a7e45431c003214febb7b6aee3f48066a3ba8dec
Crixus1125 <alexviolet11@gmail.com>
2025-05-19 21:07:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -152,4 +152,6 @@ Ahora genera la señal real con base en el gráfico e incluye el análisis técn
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
